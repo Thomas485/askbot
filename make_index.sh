f1750a374ec363608aac101d35dae2499dbbc3b8
Thomas485 <1681511+Thomas485@users.noreply.github.com>
2021-04-20 10:25:10 +0200

    rewrite make_index.sh for optimized builds

--- make_index.sh
+++ make_index.sh
@@ -1,2 +1,15 @@
-cd web
-elm make src/Main.elm --output ../index.html
+
+case $1 in
+  "optimized")
+    cd web
+    elm make src/Main.elm --optimize --output ../index.js
+    echo '<!DOCTYPE HTML>
+    <html> <head> <meta charset="UTF-8"> <title>Main</title> <script>' > ../index.html
+    uglifyjs ../index.js --compress 'pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle >> ../index.html
+    echo '</script></head> <body> <div id="myapp"></div> <script> var app = Elm.Main.init({ node: document.getElementById("myapp") }); </script> </body> </html>' >> ../index.html
+    ;;
+  "debug")
+    cd web
+    elm make src/Main.elm --debug --output ../index.html
+    ;;
+esac
